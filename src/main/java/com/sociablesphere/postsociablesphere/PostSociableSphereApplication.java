f259be6b2cccfb2fc08703db3fd295df8a375eda
jsug <jolutase104@gmail.com>
2024-10-07 12:30:02 +0200

    Initial commit

---
+++ src/main/java/com/sociablesphere/postsociablesphere/PostSociableSphereApplication.java
@@ -0,0 +1,13 @@
+package com.sociablesphere.postsociablesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSociableSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostSociableSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sociablesphere/postsociablesphere/PostSociableSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sociablesphere.postsociablesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSociableSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
