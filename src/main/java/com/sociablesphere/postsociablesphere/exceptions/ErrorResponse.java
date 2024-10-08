2070961f8b8630204a5e6c24adebba7430ce08b6
rnrv <rnrv@gft.com>
2024-10-08 11:20:37 +0200

    Controller handler

--- src/main/java/com/sociablesphere/postsociablesphere/exceptions/ErrorResponse.java
+++ src/main/java/com/sociablesphere/postsociablesphere/exceptions/ErrorResponse.java
@@ -1,4 +1,11 @@
 package com.sociablesphere.postsociablesphere.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class ErrorResponse {
+    private int code;
+    private String message;
 }
