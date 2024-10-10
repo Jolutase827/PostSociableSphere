9c56e10fe83137ba4072de560207218ea1fb72c1
jsug <jolutase104@gmail.com>
2024-10-10 09:05:58 +0200

    Changing database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,3 @@
--- Tabla Usuarios
-CREATE TABLE Usuarios (
-    id BIGSERIAL PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    photo VARCHAR(255),
-    description TEXT,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL,
-    wallet DECIMAL(10, 2),
-    api_token VARCHAR(255) UNIQUE,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-);
-
 -- Tabla posts
 CREATE TABLE posts (
     id BIGINT PRIMARY KEY NOT NULL,
@@ -33,13 +16,13 @@ CREATE TABLE posts (
 -- Tabla post_users (Relación muchos a muchos entre posts y usuarios)
 CREATE TABLE post_users (
     post_id BIGINT REFERENCES posts(id),
-    user_id BIGINT REFERENCES usuarios(id),
+    user_id BIGINT,
     PRIMARY KEY (post_id, user_id)
 );
 
 -- Tabla likes (Para registrar los likes de los usuarios a los posts)
 CREATE TABLE likes (
     post_id BIGINT REFERENCES posts(id),
-    user_id BIGINT REFERENCES usuarios(id),
+    user_id BIGINT,
     PRIMARY KEY (post_id, user_id)
 );
